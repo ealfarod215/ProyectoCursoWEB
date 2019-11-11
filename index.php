66c7b7e37a1f12b012ba7727477b4b3e7ad1fd1d
Edison <ealfarod215@ulacit.ed.cr>
2019-11-11 16:03:10 -0600

    cambio en bordes de los botones del header

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-$nombre = $ _POST['nombre'];
-$email = $ _POST['email'];
-$mensaje = $ _POST['mensaje'];
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
 $para = 'tucorreo@email.com';
 $titulo = 'Hola - La Webera';
  
 $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
  
-if ($ _POST['submit']) {
+if ($_POST['submit']) {
 if (mail ($para, $titulo, $msjCorreo)) {
 echo 'El mensaje se ha enviado';
 } else {
